6443fcc1b8625bdcb196bc61d12fcb4bd59db255
Lillian <lillian@kinemastudios.com>
2025-03-25 16:57:50 -0500

    All in one ammo fix and update ammo

--- src/mod.ts
+++ src/mod.ts
@@ -15,6 +15,7 @@ import ACPAmmoConfig from "../config/.45_ACP/config.json";
 import MAKAmmoConfig from "../config/9x18mm_Makarov/config.json";
 import PARABELLUMAmmoConfig from "../config/9x19mm_Parabellum/config.json";
 import GyurzaAmmoConfig from "../config/9x21mm_Gyurza/config.json";
+import DeagleAmmoConfig from "../config/.50_AE/config.json"
 
 //PDW Cartrides
 import HKAmmoConfig from "../config/4.6x30mm_HK/config.json";
@@ -61,7 +62,8 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
     private TTmodConfig = TTAmmoConfig;
     private MAKmodConfig = MAKAmmoConfig;
     private PARABELLUMmodConfig = PARABELLUMAmmoConfig;
-    private GYURZAmodConfig =GyurzaAmmoConfig;
+    private GYURZAmodConfig = GyurzaAmmoConfig;
+    private DeaglemodConfig = DeagleAmmoConfig;
 	
     //PDW Cartrides
     private HKmodConfig = HKAmmoConfig;
@@ -116,6 +118,7 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
         ...this.MAKmodConfig,
         ...this.PARABELLUMmodConfig,
         ...this.GYURZAmodConfig,
+        ...this.DeaglemodConfig,
         ...this.VSSmodConfig,
         ...this.ASHmodConfig,
         ...this.NSVmodConfig,
@@ -136,7 +139,181 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
         this.database = container.resolve<DatabaseServer>("DatabaseServer").getTables();
 
         // Define ammo tableCartridges as a constant.
-        const tableCartridges = ["patron_1143x23_acp", "patron_1143x23_acp_ap", "patron_1143x23_acp_hydra_shok", "patron_1143x23_acp_lasermatch_fmj", "patron_1143x23_rip", "patron_762x35_blackout", "patron_762x35_blackout_ap", "patron_762x35_m62", "patron_762x35_vmax", "patron_762x35_whisper", "patron_762x25tt_FMJ43", "patron_762x25tt_LRN", "patron_762x25tt_LRNPC", "patron_762x25tt_P_Gl", "patron_762x25tt_Pst_gzh", "patron_762x25tt_T_Gzh", "patron_762x25tt_akbs", "patron_86x70_lapua_ap", "patron_86x70_lapua_magnum", "patron_86x70_lapua_tac_x", "patron_86x70_lapua_magnum_upz", "patron_9x33r_fmj", "patron_9x33r_hp", "patron_9x33r_jhp", "patron_9x33r_sp", "patron_366_custom_ap", "patron_366_TKM_EKO", "patron_366_TKM_FMJ", "patron_366_TKM_Geksa", "patron_46x30_action_sx", "patron_46x30_ap_sx", "patron_46x30_fmj_sx", "patron_46x30_subsonic_sx", "patron_57x28_l191", "patron_57x28_r37f", "patron_57x28_r37x", "patron_57x28_sb193", "patron_57x28_ss190", "patron_57x28_ss197sr", "patron_57x28_ss198lf", "patron_545x39_7n39", "patron_545x39_7n40", "patron_545x39_BP", "patron_545x39_BS", "patron_545x39_BT", "patron_545x39_FMJ", "patron_545x39_HP", "patron_545x39_PP", "patron_545x39_PRS", "patron_545x39_PS", "patron_545x39_SP", "patron_545x39_T", "patron_545x39_US", "patron_556x45_55_FMJ", "patron_556x45_55_HP", "patron_556x45_M855", "patron_556x45_M855A1", "patron_556x45_M856", "patron_556x45_M856A1", "patron_556x45_M995", "patron_556x45_MK_255_Mod_0", "patron_556x45_mk_318_mod_0", "patron_556x45_ssa_ap", "patron_556x45_varmageddon", "patron_762x39_BP", "patron_762x39_HP", "patron_762x39_mai_ap", "patron_762x39_PS", "patron_762x39_T45M", "patron_762x39_US", "patron_762x51_bpz_fmj", "patron_762x51_M61", "patron_762x51_M80", "patron_762x51_ultra_nosler", "patron_762x51_M62", "patron_762x51_m993", "patron_762x51_tpz_sp", "patron_762х54R_7N1", "patron_762x54r_7bt1", "patron_762x54r_t46m", "patron_762х54R_LPS_Gzh", "patron_762x54r_7n37", "patron_762х54R_SNB", "patron_9x18pm_BZT_gzh", "patron_9x18pm_PBM", "patron_9x18pm_PMM", "patron_9x18pm_PPE_gzh", "patron_9x18pm_PPT_gzh", "patron_9x18pm_PRS_gs", "patron_9x18pm_PSO_gzh", "patron_9x18pm_PST_gzh", "patron_9x18pm_PSV", "patron_9x18pm_PS_gs_PPO", "patron_9x18pm_P_gzh", "patron_9x18pm_RG028_gzh", "patron_9x18pm_SP7_gzh", "patron_9x18pm_SP8_gzh", "patron_9x19_GT", "patron_9x19_luger_cci", "patron_9x19_quakemaker", "patron_9x19_rip", "patron_9x19_7n31", "patron_9x19_PSO_gzh", "patron_9x19_PST_gzh", "patron_9x19_ap_63", "patron_9x21_sp11", "patron_9x21_sp12", "patron_9x21_sp13", "patron_9x21_sp10", "patron_9x39_bp", "patron_9x39_sp5", "patron_9x39_pab9", "patron_9x39_sp6", "patron_9x39_spp", "patron_127x55_ps12", "patron_127x55_ps12a", "patron_127x55_ps12b", "patron_127x108", "patron_127x108_bzt", "patron_23x75_barricade", "patron_23x75_shrapnel_10", "patron_23x75_shrapnel_25", "patron_23x75_star", "patron_26x75_green", "patron_26x75_red", "patron_26x75_white", "patron_26x75_yellow", "patron_26x75_acidgreen", "patron_30x29_vog_30", "patron_40_VOG_25", "patron_40x46_m381", "patron_40x46_m386", "patron_40x46_m406", "patron_40x46_m433", "patron_40x46_m441", "patron_40x46_m576", "patron_12x70_buckshot", "patron_12x70_buckshot_525", "patron_12x70_buckshot_65", "patron_12x70_buckshot_85", "patron_12x70_dual_sabot_slug", "patron_12x70_flechette", "patron_12x70_rip", "patron_12x70_slug", "patron_12x70_slug_50_bmg_m17_traccer", "patron_12x70_slug_ap_20", "patron_12x70_slug_ftx_custom_lite", "patron_12x70_slug_grizzly_40", "patron_12x70_slug_hp_copper", "patron_12x70_slug_poleva_3", "patron_12x70_slug_poleva_6u", "patron_12x70_slug_superformance", "patron_20x70_buckshot", "patron_20x70_buckshot_56", "patron_20x70_buckshot_62", "patron_20x70_buckshot_73", "patron_20x70_slug_broadhead", "patron_20x70_slug_poleva_3", "patron_20x70_slug_poleva_6u", "patron_20x70_slug_star", "patron_6mm_airsoft"];
+        const tableCartridges = ["patron_1143x23_acp", 
+            "patron_1143x23_acp_ap", 
+            "patron_1143x23_acp_hydra_shok", 
+            "patron_1143x23_acp_lasermatch_fmj", 
+            "patron_1143x23_rip", 
+            "patron_762x35_blackout", 
+            "patron_762x35_cbj",
+            "patron_762x35_blackout_ap", 
+            "patron_762x35_m62", 
+            "patron_762x35_vmax", 
+            "patron_762x35_whisper", 
+            "patron_762x25tt_FMJ43", 
+            "patron_762x25tt_LRN", 
+            "patron_762x25tt_LRNPC", 
+            "patron_762x25tt_P_Gl", 
+            "patron_762x25tt_Pst_gzh", 
+            "patron_762x25tt_T_Gzh", 
+            "patron_762x25tt_akbs", 
+            "patron_86x70_lapua_ap", 
+            "patron_86x70_lapua_magnum", 
+            "patron_86x70_lapua_tac_x", 
+            "patron_86x70_lapua_magnum_upz", 
+            "patron_9x33r_fmj", 
+            "patron_9x33r_hp", 
+            "patron_9x33r_jhp", 
+            "patron_9x33r_sp", 
+            "patron_366_custom_ap", 
+            "patron_366_TKM_EKO", 
+            "patron_366_TKM_FMJ", 
+            "patron_366_TKM_Geksa", 
+            "patron_46x30_action_sx", 
+            "patron_46x30_ap_sx", 
+            "patron_46x30_fmj_sx", 
+            "patron_46x30_jsp",
+            "patron_46x30_subsonic_sx", 
+            "patron_57x28_l191", 
+            "patron_57x28_r37f", 
+            "patron_57x28_r37x", 
+            "patron_57x28_sb193", 
+            "patron_57x28_ss190", 
+            "patron_57x28_ss197sr", 
+            "patron_57x28_ss198lf", 
+            "patron_545x39_7n39", 
+            "patron_545x39_7n40", 
+            "patron_545x39_BP", 
+            "patron_545x39_BS", 
+            "patron_545x39_BT", 
+            "patron_545x39_FMJ", 
+            "patron_545x39_HP", 
+            "patron_545x39_PP", 
+            "patron_545x39_PRS", 
+            "patron_545x39_PS", 
+            "patron_545x39_SP", 
+            "patron_545x39_T", 
+            "patron_545x39_US", 
+            "patron_556x45_55_FMJ", 
+            "patron_556x45_55_HP", 
+            "patron_556x45_M855", 
+            "patron_556x45_M855A1", 
+            "patron_556x45_M856", 
+            "patron_556x45_M856A1", 
+            "patron_556x45_M995", 
+            "patron_556x45_MK_255_Mod_0", 
+            "patron_556x45_mk_318_mod_0",
+            "patron_556x45_ssa_ap", 
+            "patron_556x45_varmageddon",
+            "patron_762x39_BP", 
+            "patron_762x39_HP", 
+            "patron_762x39_mai_ap",
+            "patron_762x39_PS", 
+            "patron_762x39_T45M", 
+            "patron_762x39_US", 
+            "patron_762x39_fmj",
+            "patron_762x39_sp",
+            "patron_762x39_pp",
+            "patron_762x51_bpz_fmj", 
+            "patron_762x51_M61", 
+            "patron_762x51_M80", 
+            "patron_762x51_ultra_nosler", 
+            "patron_762x51_M62", 
+            "patron_762x51_m993", 
+            "patron_762x51_tpz_sp", 
+            "patron_762х54R_7N1", 
+            "patron_762x54r_7bt1", 
+            "patron_762x54r_t46m",
+            "patron_762х54R_LPS_Gzh",
+            "patron_762x54r_7n37", 
+            "patron_762х54R_SNB", 
+            "patron_762х54r_fmj",
+            "patron_762х54r_bthp",
+            "patron_762х54r_spbt",
+            "patron_9x18pm_BZT_gzh", 
+            "patron_9x18pm_PBM", 
+            "patron_9x18pm_PMM", 
+            "patron_9x18pm_PPE_gzh",
+            "patron_9x18pm_PPT_gzh", 
+            "patron_9x18pm_PRS_gs",
+            "patron_9x18pm_PSO_gzh",
+            "patron_9x18pm_PST_gzh",
+            "patron_9x18pm_PSV", 
+            "patron_9x18pm_PS_gs_PPO", 
+            "patron_9x18pm_P_gzh", 
+            "patron_9x18pm_RG028_gzh", 
+            "patron_9x18pm_SP7_gzh", 
+            "patron_9x18pm_SP8_gzh", 
+            "patron_9x19_GT", 
+            "patron_9x19_luger_cci", 
+            "patron_9x19_quakemaker", 
+            "patron_9x19_rip", 
+            "patron_9x19_7n31", 
+            "patron_9x19_PSO_gzh", 
+            "patron_9x19_PST_gzh", 
+            "patron_9x19_ap_63", 
+            "patron_9x19_m882",
+            "patron_9x21_sp11", 
+            "patron_9x21_sp12", 
+            "patron_9x21_sp13", 
+            "patron_9x21_sp10", 
+            "patron_9x39_bp", 
+            "patron_9x39_sp5", 
+            "patron_9x39_pab9", 
+            "patron_9x39_sp6", 
+            "patron_9x39_spp", 
+            "patron_127x55_ps12", 
+            "patron_127x55_ps12a", 
+            "patron_127x55_ps12b", 
+            "patron_127x108", 
+            "patron_127x108_bzt", 
+            "patron_23x75_barricade", 
+            "patron_23x75_shrapnel_10", 
+            "patron_23x75_shrapnel_25", 
+            "patron_23x75_star", 
+            "patron_26x75_green", 
+            "patron_26x75_red", 
+            "patron_26x75_white", 
+            "patron_26x75_yellow", 
+            "patron_26x75_acidgreen", 
+            "patron_30x29_vog_30", 
+            "patron_40_VOG_25", 
+            "patron_40x46_m381", 
+            "patron_40x46_m386", 
+            "patron_40x46_m406", 
+            "patron_40x46_m433", 
+            "patron_40x46_m441", 
+            "patron_40x46_m576", 
+            "patron_12x70_buckshot",
+            "patron_12x70_piranha",
+            "patron_12x70_buckshot_525", 
+            "patron_12x70_buckshot_65", 
+            "patron_12x70_buckshot_85", 
+            "patron_12x70_dual_sabot_slug", 
+            "patron_12x70_flechette", 
+            "patron_12x70_rip", 
+            "patron_12x70_slug", 
+            "patron_12x70_slug_50_bmg_m17_traccer", 
+            "patron_12x70_slug_ap_20", 
+            "patron_12x70_slug_ftx_custom_lite", 
+            "patron_12x70_slug_grizzly_40", 
+            "patron_12x70_slug_hp_copper", 
+            "patron_12x70_slug_poleva_3", 
+            "patron_12x70_slug_poleva_6u", 
+            "patron_12x70_slug_superformance", 
+            "patron_20x70_buckshot", 
+            "patron_20x70_buckshot_56", 
+            "patron_20x70_buckshot_62", 
+            "patron_20x70_buckshot_73", 
+            "patron_20x70_slug_broadhead", 
+            "patron_20x70_slug_poleva_3", 
+            "patron_20x70_slug_poleva_6u", 
+            "patron_20x70_slug_star", 
+            "patron_127x33_fmj",
+            "patron_127x33_jhp",
+            "patron_127x33_cooper",
+            "patron_127x33_jsp",
+            "patron_6mm_airsoft"];
 		
         this.logger.warning("\n[C.A.T] Processing Enabled Cartridges.");
 		
@@ -150,7 +327,7 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
             }
         }
 		
-        if (this.AmmoChanged == 0)
+        if (this.AmmoChanged === 0)
         {
             this.logger.warning("\n[C.A.T] No Cartridges Are Enabled For Processing.");
         }
@@ -171,7 +348,7 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
             const trader = this.database.traders;
             const items = this.database.templates.items;
 			
-            if (tyrCartBlinding == false) 
+            if (tyrCartBlinding === false) 
             {
                 const TyrAllInOne: NewItemFromCloneDetails = {
                     itemTplToClone: "5fc382a9d724d907e2077dab",
@@ -208,7 +385,8 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
                         MaxExplosionDistance: TyrianAllInOneCartridge.MaxExplosionDistance,
                         ExplosionType: "big_round_impact_explosive"
                     },
-                    newId: "patron_Tyr_AllInOne",
+                    
+                    newId: "67e320581f6bd18daca60634",
                     parentId: "5485a8684bdc2da71d8b4567",
                     handbookParentId: "5b47574386f77428ca22b33b",
                     fleaPriceRoubles: 1,
@@ -223,7 +401,7 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
                 };
                 customItem.createItemFromClone(TyrAllInOne);
             }
-            if (tyrCartBlinding == true) 
+            if (tyrCartBlinding === true) 
             {
                 const TyrAllInOne: NewItemFromCloneDetails = {
                     itemTplToClone: "5fc382a9d724d907e2077dab",
@@ -262,14 +440,14 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
                         Contusion:{"x":1.5, "y":4, "z":15},
                         Blindness:{"x":10, "y":20, "z":20}
                     },
-                    newId: "patron_Tyr_AllInOne",
+                    newId: "67e320581f6bd18daca60634",
                     parentId: "5485a8684bdc2da71d8b4567",
                     handbookParentId: "5b47574386f77428ca22b33b",
                     fleaPriceRoubles: 1,
                     handbookPriceRoubles: 1,
                     locales: {
                         "en": {
-                            name: "Tyrians All In One Special Ammo",
+                            name: "All In One Special Ammo",
                             shortName: "TyrAmmo",
                             description: "A Super Special Ammo Type for All Weapons and Magazines."
                         }
@@ -283,8 +461,8 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
 			
             assort.items.push(
                 {
-                    _id: "patron_Tyr_AllInOne",
-                    _tpl: "patron_Tyr_AllInOne",
+                    _id: "67e320581f6bd18daca60634",
+                    _tpl: "67e320581f6bd18daca60634",
                     parentId: "hideout",
                     slotId: "hideout",
                     upd: {
@@ -292,8 +470,8 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
                     }
                 }
             );
-            assort.loyal_level_items["patron_Tyr_AllInOne"] = 1;
-            assort.barter_scheme["patron_Tyr_AllInOne"] = [
+            assort.loyal_level_items["67e320581f6bd18daca60634"] = 1;
+            assort.barter_scheme["67e320581f6bd18daca60634"] = [
                 [
                     {
                         _tpl: "5449016a4bdc2d6f028b456f",
@@ -313,18 +491,18 @@ class ComprehensiveAmmunitionTweaker implements IPostDBLoadMod
                         {
                             for (const slot of cylinderSlots) 
                             {
-                                slot._props.filters[0].Filter.push("patron_Tyr_AllInOne");
+                                slot._props.filters[0].Filter.push("67e320581f6bd18daca60634");
                             }
                         }
 
-                        item._props.Cartridges[0]._props.filters[0].Filter.push("patron_Tyr_AllInOne");
+                        item._props.Cartridges[0]._props.filters[0].Filter.push("67e320581f6bd18daca60634");
                     }
 
                     if (this.isSet(item._props.Chambers)) 
                     {
                         for (const chamber of item._props.Chambers) 
                         {
-                            chamber._props.filters[0].Filter.push("patron_Tyr_AllInOne");
+                            chamber._props.filters[0].Filter.push("67e320581f6bd18daca60634");
                         }
                     }
                 }
